ac8975cca9b0957c51a770c82cdaccc76d327057
Robert Walter <rwalter83@gmail.com>
2021-02-27 15:08:20 +0100

    add authorization (close #3)

--- README.md
+++ README.md
@@ -10,21 +10,31 @@ Add your custom command definitions to `config.json`:
 {
     "host": "0.0.0.0",
     "port": 8098,
+    "secret_key": null,
     "commands": {
         "shutdown": "systemctl poweroff -i",
         "ping": "ping -c 3 google.de"
     }
 }
 ```
+If you want to enable authorization, set `secret_key` to a random secret value e.g. using:
+```bash
+$ python3 -c 'import secrets; print(secrets.token_urlsafe())'
+```
 
 ## Usage
 1. `$ ./run.sh`
-1. Open `http://<your_ip>:8098/` in the browser (e.g. from your phone)
-1. Remotely run your commands by tapping a button while observing stdout/stderr in realtime
+2. Open `http://<your_ip>:8098/` in the browser (e.g. from your phone)
+3. If authorization is enabled, the server terminal will prompt `Accept session from <remote_ip>? [y,N]`. After confirming with 'y', the client will be authenticated automatically and authorized permanently in future sessions. After denying by any other input than 'y', the client will be rejected automatically until the server is restarted.
+4. Remotely run your commands by tapping a button while observing stdout/stderr in realtime
 
 <p align="center">
     <img src="https://user-images.githubusercontent.com/609855/109354034-78ed6100-787d-11eb-9106-87c7bf6d692d.png" alt="screenshot" height="640">
 </p>
+
+## Notes
+- The intended use case is **not** public deployment, but rather convenience in closed home network environments
+- It won't work with older browsers without explicitly setting same-origin credentials (due to the use of [fetch()](https://github.com/whatwg/fetch/pull/585))
 
 
 
--- server.py
+++ server.py
@@ -1,20 +1,45 @@
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, request, session
 import json
 from subprocess import Popen, PIPE, STDOUT
+from datetime import timedelta
 
 app = Flask(__name__)
 config = json.load(open('config.json', 'r'))
+app.secret_key = config['secret_key']
+app.permanent_session_lifetime = timedelta(days=365)
+denyset = set()
+
+def authorized():
+  if not app.secret_key:
+    return True
+  if request.remote_addr in denyset:
+    return False
+  if 'authorized' in session:
+    return session['authorized']
+  else:
+    accecpt = input(f'Accept session from {request.remote_addr}? [y,N]: ')
+    if accecpt in ['y', 'Y']:
+      session.permanent = True      
+      session['authorized'] = True
+      return True
+    else:
+      denyset.add(request.remote_addr)
+      return False
 
 @app.route('/run/<command>')
 def run(command):
-    def generate():
-        with Popen(config['commands'][command], shell=True, stdout=PIPE, stderr=STDOUT) as p:
-          while (line := p.stdout.readline()):
-            yield line
-    return Response(generate(), mimetype='text/html')
+  if not authorized():
+    return "Unauthorized"
+  def generate():
+      with Popen(config['commands'][command], shell=True, stdout=PIPE, stderr=STDOUT) as p:
+        while (line := p.stdout.readline()):
+          yield line
+  return Response(generate(), mimetype='text/html')
 
 @app.route('/')
 def index():
+  if not authorized():
+    return "Unauthorized"
   return render_template('commands.j2', commands = map(lambda k: dict(name=k, cmd=config['commands'][k]) , config['commands']))
 
 if __name__ == '__main__':
